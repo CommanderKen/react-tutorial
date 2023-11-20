c5ff3043d2c247baf2f50bf39f6bdfbab8ca1c49
CommanderKen <commander_keen01@proton.me>
2023-11-20 17:56:10 +0100

    Calculate winner

--- src/App.js
+++ src/App.js
@@ -3,23 +3,38 @@ import { useState } from 'react';
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [next, setNext] = useState('X');
+  const [status, setStatus] = useState('Next player is: X');
 
   function handleClick(index) {
-    if (squares[index]) {
+    if (squares[index] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
     nextSquares[index] = next;
+    const actual = next;
     if(next === 'X') {
       setNext('O');
+      setStatus('Next player is: O');
     } else {
       setNext('X');
+      setStatus('Next player is: X');
     }
     setSquares(nextSquares);
+    if (calculateWinner(nextSquares)) {
+      setStatus('Winner is: ' + actual);
+    }
+  }
+
+  function restart() {
+    setSquares(Array(9).fill(null));
+    setNext('X');
+    setStatus('Next player is: X');
   }
 
   return (
     <>
+      <div className="status">{status}</div>
+      <button className="restart" onClick={restart}>Restart</button>
       <div className="board-row">
         <Square value={squares[0]} handleSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} handleSquareClick={() => handleClick(1)} />
@@ -43,4 +58,19 @@ function Square({value, handleSquareClick}) {
   return (
     <button className="square" onClick={handleSquareClick}>{value}</button>
   )
+}
+
+function calculateWinner(squares) {
+  console.log(squares[0] == squares[1] == squares[2]);
+  if ( (squares[0] === squares[1] && squares[1] === squares[2] && squares[0] !== null)
+    || (squares[3] === squares[4] && squares[4] === squares[5] && squares[3] !== null)
+    || (squares[6] === squares[7] && squares[7] === squares[8] && squares[6] !== null)
+    || (squares[0] === squares[3] && squares[3] === squares[6] && squares[0] !== null)
+    || (squares[1] === squares[4] && squares[4] === squares[7] && squares[1] !== null)
+    || (squares[2] === squares[5] && squares[5] === squares[8] && squares[2] !== null)
+    || (squares[0] === squares[4] && squares[4] === squares[8] && squares[0] !== null)
+    || (squares[2] === squares[4] && squares[4] === squares[6] && squares[2] !== null)) {
+    return true;
+  }
+  return false;
 }
