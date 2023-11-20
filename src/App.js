d1781f32dd660e15473e2f5b117cee8058ed2df8
CommanderKen <commander_keen01@proton.me>
2023-11-20 17:19:13 +0100

    Added board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-export default function Square() {
+import { useState } from 'react';
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [next, setNext] = useState('X');
+
+  function handleClick(index) {
+    if (squares[index]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[index] = next;
+    if(next === 'X') {
+      setNext('O');
+    } else {
+      setNext('X');
+    }
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <button className="square">0</button>
-        <button className="square">1</button>
-        <button className="square">2</button>
+        <Square value={squares[0]} handleSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} handleSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} handleSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <button className="square">3</button>
-        <button className="square">4</button>
-        <button className="square">5</button>
+        <Square value={squares[3]} handleSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} handleSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} handleSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <button className="square">6</button>
-        <button className="square">7</button>
-        <button className="square">8</button>
+        <Square value={squares[6]} handleSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} handleSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} handleSquareClick={() => handleClick(8)} />
       </div>
     </>
+  )
+}
+
+function Square({value, handleSquareClick}) {
+  return (
+    <button className="square" onClick={handleSquareClick}>{value}</button>
   )
 }
